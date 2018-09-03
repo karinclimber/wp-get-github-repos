4da9274ddce4562211424c4ef3714b463b400c47
Adam Lea <adamlea@Adams-MacBook-Pro.local>
2018-09-03 11:23:34 -0600

    complete initial setup

---
+++ wp-get-github-repos.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+* Plugin Name: WP Get Github Repos
+* Descripton: Pulls my GUthub Repos to display in sidebar widget
+* Version: 1.0
+* Author: Adam John Lea
+*/
+
+// Exit if accesed directly
+if(!defined('ABSPATH')) {
+    exit;
+}
+
