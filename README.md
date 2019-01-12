66f9fed0158e0530b53b2fa905a7f8fccb0373b3
adamjohnlea <imnogeekdptcom@me.com>
2019-01-12 11:34:41 -0700

    improve the README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WP-Get-Github-Repos
+
+## Tested with WP 5.0+
+
 A WordPress plugin that allows you to return x number of repos from your Github account and display them in a sidebar widget.
--- wp-get-github-repos.php
+++ wp-get-github-repos.php
@@ -1,7 +1,7 @@
 <?php 
 /*
 * Plugin Name: WP Get Github Repos
-* Descripton: Pulls my GUthub Repos to display in sidebar widget
+* Descripton: Pulls my Github Repos to display in sidebar widget
 * Version: 1.0
 * Author: Adam John Lea
 */
